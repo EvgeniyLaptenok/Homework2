290122eef66a3624fe8e2bea189fc762bc5cbbd3
evgeniy <shtorm20300@gmail.com>
2022-12-05 12:46:51 +0600

    задача_2

---
+++ z2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+string num = Console.ReadLine()!;
+int number = int.Parse(num);
+int[] array = new int[num.Length];
+
+if (number > 99)
+{
+    Console.WriteLine(num[2]);
+}
+else
+{
+    Console.WriteLine("Нет третьего числа");
+}
+   
