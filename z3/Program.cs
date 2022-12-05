45583022c7255c56d6ba68b3126e866753f84124
evgeniy <shtorm20300@gmail.com>
2022-12-05 13:47:14 +0600

    задача_3

---
+++ z3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int fri = 6;
+int sat = 7;
+
+if (num == fri || num == sat)
+{
+    
+    Console.WriteLine("Выходной день");
+}
+else if (num < 1 || num >= 8)
+    {
+        Console.WriteLine("Неверное значение");
+    }
+else
+{
+    Console.WriteLine("Будний день");
+}
