248ecee397552110dfe5d19431d1b6315fc78158
evgeniy <shtorm20300@gmail.com>
2022-12-05 11:40:28 +0600

    задача_1

---
+++ z1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int result = ((num - (num % 10)) % 100) / 10;
+
+if (num < 100 || num > 999)
+{
+    Console.WriteLine("Неверное значение");
+}
+else
+{
+    Console.WriteLine(result);
+}
